c93b89a18541f336d1fe78479215d26b06f96e11
Zamira <keldibaevaz@gmail.com>
2025-05-13 21:55:24 +0300

    13.05.2025

---
+++ src/main/java/levelOne/task/Task3.java
@@ -0,0 +1,21 @@
+package levelOne.task;
+
+/**
+ * Имеется целое число, вводимое с консоли.
+ * Это число сумма денег в рублях
+ * Вывести это число, добавив к нему слово рубль
+ * в правильном падеже (рубль, рублей, рубля)
+ */
+public class Task3 {
+    public static void main(String[] args) {
+        int rubles = 112;
+        if (rubles % 10 == 1 && rubles % 100 != 11) {
+            System.out.println(rubles + " рубль");
+        } else if ((rubles % 10 == 2 || rubles % 10 == 3 || rubles % 10 == 4)
+                  && !(rubles % 100 == 12 || rubles % 100 == 13 || rubles % 100 == 14)) {
+            System.out.println(rubles + " рубля");
+        } else {
+            System.out.println(rubles + " рублей");
+        }
+    }
+}
