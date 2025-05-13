29aa9b94237263788c597ccc835690fd95210f12
Zamira <keldibaevaz@gmail.com>
2025-05-13 21:25:39 +0300

    13.05.2025

---
+++ src/main/java/levelOne/Lesson_2_3.java
@@ -0,0 +1,14 @@
+package levelOne;
+
+public class Lesson_2_3 {
+    public static void main(String[] args) {
+        int value = 0;
+        if (value > 0) {
+            System.out.println("true");
+        } else if (value < 0) {
+            System.out.println("false");
+        } else {
+            System.out.println("its zero");
+        }
+    }
+}
---
+++ src/main/java/levelOne/Lesson_2_4.java
@@ -0,0 +1,17 @@
+package levelOne;
+
+public class Lesson_2_4 {
+    public static void main(String[] args) {
+        int value = 12;
+        switch (value) {
+            case 1:
+                System.out.println("value == 1");
+                break;
+            case 2:
+                System.out.println("value == 2");
+                break;
+            default:
+                System.out.println("not anything");
+        }
+    }
+}
