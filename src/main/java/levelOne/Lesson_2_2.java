2fd1b2cb91327cdea3be8722b169d2b977ddd1ba
Zamira <keldibaevaz@gmail.com>
2025-05-13 21:12:03 +0300

    13.05.2025

---
+++ src/main/java/levelOne/Lesson_2_2.java
@@ -0,0 +1,19 @@
+package levelOne;
+
+public class Lesson_2_2 {
+    public static void main(String[] args) {
+        int value = 5;
+        int value2 = 3;
+        int sum = value2 + value;
+        System.out.println(sum);
+
+        int value3 = 5;
+        value3 = value3 + 25;
+        System.out.println(value3);
+        //либо упростить
+        value3 += 25;
+        System.out.println(value3);
+
+
+    }
+}
