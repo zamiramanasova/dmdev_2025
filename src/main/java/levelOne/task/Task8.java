953ce9a7a5393587e493e393cbafa4807cd18c31
Zamira <keldibaevaz@gmail.com>
2025-05-20 21:03:28 +0300

    20.05.2025

---
+++ src/main/java/levelOne/task/Task8.java
@@ -0,0 +1,36 @@
+package levelOne.task;
+
+/**
+ * Имеется число. Посчитать сумму данного числа.
+ */
+public class Task8 {
+    public static void main(String[] args) {
+        int value = 12345;
+        System.out.println(sum(value));
+        System.out.println("======");
+        System.out.println(sum2(value));
+    }
+    // здесь считает так, если даешь 5 то 1+2+3+4+5 = 15
+    public static int sum(int value) {
+        int result = 0;
+        for(int i = 0; i <= value; i++) {
+            result += i;
+        }
+        return result;
+    }
+
+    //здесь если даешь 123 = 1+2+3 = 6, если даешь 5 то даст 5
+    public static int sum2(int value) {
+        int result = 0;
+        int currentValue = value;
+        while (currentValue != 0) {
+            //записали после остатка от 10 в резалт
+            result += currentValue % 10;
+            //после этого делим каррентвэлъю на 10
+            currentValue /= 10;
+            //потом опять по циклю с этим числом
+            //каррент дальше крутим пока не станет нулем
+        }
+        return result;
+    }
+}
