1280df526bafcf5b9db92b1f1ce09dbbec4cef4d
Zamira <keldibaevaz@gmail.com>
2025-05-20 21:24:24 +0300

    20.05.2025

--- src/main/java/levelOne/task/Task8.java
+++ src/main/java/levelOne/task/Task8.java
@@ -5,16 +5,19 @@ package levelOne.task;
  */
 public class Task8 {
     public static void main(String[] args) {
-        int value = 12345;
+        int value = 12;
         System.out.println(sum(value));
         System.out.println("======");
         System.out.println(sum2(value));
+        System.out.println("======");
+        System.out.println(sum3(value));
     }
     // здесь считает так, если даешь 5 то 1+2+3+4+5 = 15
+    // 1+2+3+4+5+6+7+8+9+10+11+12 = 78
     public static int sum(int value) {
         int result = 0;
         for(int i = 0; i <= value; i++) {
-            result += i;
+             result += i;
         }
         return result;
     }
@@ -30,6 +33,15 @@ public class Task8 {
             currentValue /= 10;
             //потом опять по циклю с этим числом
             //каррент дальше крутим пока не станет нулем
+        }
+        return result;
+    }
+
+    //здесь если даешь 123 = 1+2+3 = 6, если даешь 5 то даст 5
+    public static int sum3(int value) {
+        int result = 0;
+        for (int currentValue = value; currentValue != 0; currentValue /= 10) {
+            result += currentValue % 10;
         }
         return result;
     }
