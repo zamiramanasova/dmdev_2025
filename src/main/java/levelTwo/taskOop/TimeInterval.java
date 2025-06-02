d4900b259d3b144d86c82b2260008f8898f278fc
Zamira <keldibaevaz@gmail.com>
2025-06-02 22:24:49 +0300

    02.06.2025

--- src/main/java/levelTwo/taskOop/TimeInterval.java
+++ src/main/java/levelTwo/taskOop/TimeInterval.java
@@ -5,9 +5,26 @@ public class TimeInterval {
     private int minutes;
     private int hours;
 
+    public TimeInterval(int totalSeconds) {
+        this.hours = totalSeconds / 3600;
+        this.minutes = totalSeconds % 3600 / 60;
+        this.seconds = totalSeconds % 60 % 60;
+    }
+
     public TimeInterval(int seconds, int minutes, int hours) {
         this.seconds = seconds;
         this.minutes = minutes;
         this.hours = hours;
+    }
+
+    public int totalSeconds() {
+        return seconds + minutes * 60 + hours * 60 * 60;
+    }
+    public void  print() {
+        System.out.println("h " + hours + "m " + minutes + "s " + seconds);
+    }
+
+    public TimeInterval sum(TimeInterval second) {
+        return new TimeInterval(totalSeconds() + second.totalSeconds());
     }
 }
--- src/main/java/levelTwo/taskOop/TimeIntervalRunner.java
+++ src/main/java/levelTwo/taskOop/TimeIntervalRunner.java
@@ -1,4 +1,16 @@
 package levelTwo.taskOop;
 
 public class TimeIntervalRunner {
+    public static void main(String[] args) {
+        TimeInterval timeInterval = new TimeInterval(30, 2, 1);
+        System.out.println(timeInterval.totalSeconds());
+
+        TimeInterval timeInterval1 = new TimeInterval(timeInterval.totalSeconds());
+        System.out.println(timeInterval1.totalSeconds());
+
+        timeInterval1.print();
+
+        TimeInterval sumIntervals = timeInterval.sum(timeInterval1);
+        sumIntervals.print();
+    }
 }
