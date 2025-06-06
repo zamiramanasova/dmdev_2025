b392e579bc5f6e59df736c9283669a20bc20cebf
Zamira <keldibaevaz@gmail.com>
2025-06-06 23:44:32 +0300

    06.06.2025

--- src/main/java/levelTwo/taskOop/TimeInterval.java
+++ src/main/java/levelTwo/taskOop/TimeInterval.java
@@ -1,6 +1,8 @@
 package levelTwo.taskOop;
 
 public class TimeInterval {
+    private static final int SECONDS_IN_MINUTE = 60;
+    private static final int MINUTES_IN_HOUR = 60;
     private int seconds;
     private int minutes;
     private int hours;
@@ -18,7 +20,7 @@ public class TimeInterval {
     }
 
     public int totalSeconds() {
-        return seconds + minutes * 60 + hours * 60 * 60;
+        return seconds + minutes * SECONDS_IN_MINUTE + hours * SECONDS_IN_MINUTE * MINUTES_IN_HOUR;
     }
     public void  print() {
         System.out.println("h " + hours + "m " + minutes + "s " + seconds);
