ec97c7c574966cc497b39e550abacdd0c2f2503d
Zamira <keldibaevaz@gmail.com>
2025-06-12 10:11:37 +0300

    12.06.2025

--- src/main/java/levelTwo/composition/OopLessonRunner.java
+++ src/main/java/levelTwo/composition/OopLessonRunner.java
@@ -2,15 +2,22 @@ package levelTwo.composition;
 
 public class OopLessonRunner {
     public static void main(String[] args) {
-        Computer lapTop = new LapTop(new Ssd(250), new Ram(1024), 2);
-        Computer mobile = new Mobile(new Ssd(250), new Ram(1024));
-        print(lapTop, mobile);
+        Printable lapTop = new LapTop(new Ssd(250), new Ram(1024), 2);
+        Printable mobile = new Mobile(new Ssd(250), new Ram(1024));
+        printWithRandom(lapTop, mobile);
 
     }
 
     public static void print(Printable... objects) {
         for (Printable object : objects) {
             object.print();
+            System.out.println();
+        }
+    }
+
+    public static void printWithRandom(Printable... objects) {
+        for (Printable object : objects) {
+            object.printWithRandom();
             System.out.println();
         }
     }
--- src/main/java/levelTwo/composition/Printable.java
+++ src/main/java/levelTwo/composition/Printable.java
@@ -1,7 +1,17 @@
 package levelTwo.composition;
 
+import java.util.Random;
+
 public interface Printable {
     String SOME_VALUE = "Example";
+    Random RANDOM = new Random();
+    default void printWithRandom() {
+        System.out.println(RANDOM.nextInt());
+        print();
+    }
     void print();
 
+    private int generateRandom() {
+        return RANDOM.nextInt();
+    }
 }
