7a279a371fd99d7b4da1a7ad413f5741afb40633
Zamira <keldibaevaz@gmail.com>
2025-06-19 20:09:45 +0300

    19.06.2025

---
+++ src/main/java/levelTwo/hashcode/LinkedHashMapExample.java
@@ -0,0 +1,23 @@
+package levelTwo.hashcode;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LinkedHashMapExample {
+    public static void main(String[] args) {
+
+        Person ivan = new Person(1,"Ivan", "Ivanov");
+        Person petr = new Person(29,"Petr", "Petrov");
+        Person sveta = new Person(3,"Sveta", "Svetikova");
+
+        Map<Integer, Person> map = new LinkedHashMap<>();
+        map.put(ivan.getId(), ivan);
+        map.put(petr.getId(), petr);
+        map.put(sveta.getId(), sveta);
+
+        for (Map.Entry<Integer, Person> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+}
