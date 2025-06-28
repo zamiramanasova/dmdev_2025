68c572ad5cc798e14d0be2ea3643a0682e52c1c3
Zamira <keldibaevaz@gmail.com>
2025-06-28 16:01:28 +0300

    28.06.2025

--- src/main/java/levelTwo/exception/ExceptionExample.java
+++ src/main/java/levelTwo/exception/ExceptionExample.java
@@ -7,12 +7,24 @@ public class ExceptionExample {
     public static void main(String[] args) {
         System.out.println("main start");
         try {
+            System.out.println(finallyTest());
             unsafe(-10);
         } catch (Exception exception) {
             exception.printStackTrace();
+        } finally {
+            System.out.println("finally");
         }
         System.out.println("main end");
 
+    }
+    public static int finallyTest() {
+        try {
+            return 2;
+        } catch (Throwable throwable) {
+            return 3;
+        } finally {
+            return 4;
+        }
     }
 
     public static void unsafe(int value) throws FileNotFoundException, TimeoutException {
