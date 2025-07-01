c97b9e6907cccaa15b7aa788389d73b1e45766a6
Zamira <keldibaevaz@gmail.com>
2025-07-01 22:44:28 +0300

    01.07.2025

---
+++ src/main/java/levelTwo/lambda/Student.java
@@ -0,0 +1,19 @@
+package levelTwo.lambda;
+
+public class Student {
+    private int age;
+    private String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
