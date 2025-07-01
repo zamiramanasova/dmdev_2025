ab67429eb703dd3e03ab25aa2faf7e4821541dad
Zamira <keldibaevaz@gmail.com>
2025-07-01 22:41:34 +0300

    01.07.2025

--- src/main/java/levelTwo/lambda/StreamExample.java
+++ src/main/java/levelTwo/lambda/StreamExample.java
@@ -1,9 +1,14 @@
 package levelTwo.lambda;
 
 import java.util.List;
+import java.util.function.IntUnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamExample {
     public static void main(String[] args) {
+        System.out.println("1 ==========");
         //до стримов
         List<String> strings = List.of("88", "11", "22", "33", "44", "55", "66");
         //STREAM
@@ -12,9 +17,30 @@ public class StreamExample {
                 .map(Integer::valueOf)
                 .filter(value -> value % 2 == 0)
                 .sorted()
-                .skip(1)
+                //.skip(1)
                 .limit(2)
+                .mapToInt(Integer::intValue)
+                //.forEach(System.out::println);
+                .summaryStatistics();
+        System.out.println(strings);
+        System.out.println("2 ==========");
+
+        List<String> collect = Stream.of("88", "11", "22", "33", "44", "55", "66")
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+
+        System.out.println("3 ==========");
+
+        IntStream.range(0, 10)
                 .forEach(System.out::println);
+
+        System.out.println("4 ==========");
+        IntStream.iterate(0, operand -> operand + 3)
+                .skip(10)
+                .limit(20)
+                .forEach(System.out::println);
+
+        System.out.println("5 ==========");
 
         for (String string : strings) {
             String value = string + string;
