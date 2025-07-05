b09759403e03535aa88bf3eda55b0b76845662b5
Zamira <keldibaevaz@gmail.com>
2025-07-05 16:44:24 +0300

    05.07.2025

--- src/main/java/levelTwo/lambda/optional/Task2.java
+++ src/main/java/levelTwo/lambda/optional/Task2.java
@@ -1,4 +1,37 @@
 package levelTwo.lambda.optional;
 
+/**
+ * Дан список строк. Найти количество уникальных
+ * строк блиной более 8 символов
+ */
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Task2 {
+    public static void main(String[] args) {
+        List<String> strings = List.of(
+                "string-1",
+                "string-2",
+                "string-3",
+                "string-4",
+                "string-10",
+                "string-10",
+                "string-10",
+                "string-12",
+                "string-16"
+        );
+        int result = strings.stream()
+                .filter(value -> value.length() > 8)
+                .collect(Collectors.toSet())
+                .size();
+        System.out.println(result);
+
+        //second variant
+        long result2 = strings.stream()
+                .filter(value -> value.length() > 8)
+                .distinct()
+                .count();
+        System.out.println(result2);
+    }
 }
