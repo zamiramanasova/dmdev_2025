652d1294ce28923dc3caf2815cfeee6d52053d48
Zamira <keldibaevaz@gmail.com>
2025-07-05 16:01:27 +0300

    05.07.2025

--- src/main/java/levelTwo/lambda/StreamExample.java
+++ src/main/java/levelTwo/lambda/StreamExample.java
@@ -1,7 +1,6 @@
 package levelTwo.lambda;
 
 import java.util.List;
-import java.util.function.IntUnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
--- src/main/java/levelTwo/lambda/Student.java
+++ src/main/java/levelTwo/lambda/Student.java
@@ -16,4 +16,12 @@ public class Student {
     public String getName() {
         return name;
     }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
