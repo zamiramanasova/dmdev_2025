10badedba7e7b5b4dc87aa2db310ff2f12ca98fe
Zamira <keldibaevaz@gmail.com>
2025-07-07 21:31:00 +0300

    07.07.2025

---
+++ src/main/java/levelTwo/fileLesson/FileRunner.java
@@ -0,0 +1,23 @@
+package levelTwo.fileLesson;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * input stream - введение инфы
+ * output stream - вывод инфы
+ */
+public class FileRunner {
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("resources/testFile.txt");
+        System.out.println(file.createNewFile());
+        System.out.println(file.exists());
+        System.out.println(file.isFile());
+        System.out.println(file.isDirectory());
+        System.out.println(file.getName());
+        System.out.println(file.getParent());
+        System.out.println(file.getAbsolutePath());
+        System.out.println(file.getAbsoluteFile());
+    }
+}
