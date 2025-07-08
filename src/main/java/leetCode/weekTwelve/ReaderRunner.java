709a0f6b99c2809a5b27eeebaf6e9b95a81eb501
Zamira <keldibaevaz@gmail.com>
2025-07-08 22:12:16 +0300

    08.07.2025

---
+++ src/main/java/leetCode/weekTwelve/ReaderRunner.java
@@ -0,0 +1,16 @@
+package leetCode.weekTwelve;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+
+public class ReaderRunner {
+    public static void main(String[] args) throws IOException {
+        File file = Path.of("resources", "test.txt").toFile();
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(file))) {
+            String collect = fileReader.lines()
+                    .collect(Collectors.joining("\n"));
+            System.out.println(collect);
+        }
+    }
+}
