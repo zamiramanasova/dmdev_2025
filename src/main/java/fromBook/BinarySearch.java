37ec3d770f3855d264e5709116f04841036f8428
Zamira <keldibaevaz@gmail.com>
2025-07-09 22:21:47 +0300

    09.07.2025

--- src/main/java/fromBook/BinarySearch.java
+++ src/main/java/fromBook/BinarySearch.java
@@ -11,13 +11,20 @@ public class BinarySearch {
         int low = 0;
         int high = list.length - 1;
 
+        //пока эта часть не сократится до одного
+        //элемента, проверяем средний элемент
         while (low <= high) {
+            //если значение нечетно, то округляем mid в меньшую сторону
             int mid = (low + high) / 2;
+            //если названное число было слишком мало, то переменная low обновляется соответственно
             int guess = list[mid];
+            //если значение найдено то возращаем средний элемент
             if (guess == item)
                 return mid;
+            //если значение много то high = mid - 1
             if (guess > item)
                 high = mid - 1;
+            //иначе
             else
                 low = mid + 1;
             return 0;
