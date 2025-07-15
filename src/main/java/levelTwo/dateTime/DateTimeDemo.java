9309e866acc5cdd0d71757cb128e74882c087bce
Zamira <keldibaevaz@gmail.com>
2025-07-15 21:15:35 +0300

    15.07.2025

---
+++ src/main/java/levelTwo/dateTime/DateTimeDemo.java
@@ -0,0 +1,10 @@
+package levelTwo.dateTime;
+
+import java.time.ZonedDateTime;
+
+public class DateTimeDemo {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
