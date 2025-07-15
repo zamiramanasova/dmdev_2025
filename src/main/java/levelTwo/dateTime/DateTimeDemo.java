9e7180ded4da158fd797e587e00c63820c47b8f8
Zamira <keldibaevaz@gmail.com>
2025-07-15 21:17:17 +0300

    15.07.2025

--- src/main/java/levelTwo/dateTime/DateTimeDemo.java
+++ src/main/java/levelTwo/dateTime/DateTimeDemo.java
@@ -1,10 +1,17 @@
 package levelTwo.dateTime;
 
 import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class DateTimeDemo {
     public static void main(String[] args) {
         ZonedDateTime now = ZonedDateTime.now();
         System.out.println(now);
+
+        long toEpochMilli = now.toInstant().toEpochMilli();
+        System.out.println(toEpochMilli);
+
+        ZonedDateTime plus = now.plus(1L, ChronoUnit.DAYS);
+        System.out.println(plus);
     }
 }
