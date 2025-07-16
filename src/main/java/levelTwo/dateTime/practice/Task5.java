479f5892b8baca4e216a7425cee8e1c2055ceee4
Zamira <keldibaevaz@gmail.com>
2025-07-16 18:24:28 +0300

    16.07.2025

---
+++ src/main/java/levelTwo/dateTime/practice/Task5.java
@@ -0,0 +1,26 @@
+package levelTwo.dateTime.practice;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * Создать объект LocalDate, представляющий собой сегоднящнюю дату.
+ * Создать объект LocalDate, представляющий собой дату 07.07.2018.
+ * Используя созданные объекты, найти количество
+ * дней между этими двумя датами
+ */
+public class Task5 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        LocalDate prevDate = LocalDate.of(2018, 7, 7);
+
+        Period period = Period.between(prevDate, now);
+        System.out.println(period.getYears());
+        System.out.println(period.getMonths());
+        System.out.println(period.getDays());
+        long days = ChronoUnit.DAYS.between(prevDate, now);
+        System.out.println(days);
+    }
+}
