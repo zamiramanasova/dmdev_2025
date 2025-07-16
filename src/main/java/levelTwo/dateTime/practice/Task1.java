40ca3e46a1abc654e63ec59bc3b91445cc76c463
Zamira <keldibaevaz@gmail.com>
2025-07-16 17:53:08 +0300

    16.07.2025

---
+++ src/main/java/levelTwo/dateTime/practice/Task1.java
@@ -0,0 +1,11 @@
+package levelTwo.dateTime.practice;
+
+/**
+ * Создать объект LocalDateTime представляющий собой дату
+ * 25.06.2020 19:47.
+ * Используя этот объект создать другой объект LocalDateTime
+ * представляющий собой дату через 3 месяца после сегодняшней.
+ * Вывести на консоль содержащиеся в нем LocalDate and LocalTime
+ */
+public class Task1 {
+}
