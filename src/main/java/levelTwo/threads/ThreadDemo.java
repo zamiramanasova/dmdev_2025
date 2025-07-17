c540976da6d0ebb199dd5fe67e5d01c54d740cc3
Zamira <keldibaevaz@gmail.com>
2025-07-17 17:24:10 +0300

    17.07.2025

--- src/main/java/levelTwo/threads/ThreadDemo.java
+++ src/main/java/levelTwo/threads/ThreadDemo.java
@@ -4,6 +4,11 @@ public class ThreadDemo {
     public static void main(String[] args) {
         SimpleThread simpleThread = new SimpleThread();
         simpleThread.start();
+        try {
+            simpleThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println(Thread.currentThread().getName());
     }
 }
