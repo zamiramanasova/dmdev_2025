1eb95cb25b759d648d35fb790d5b1912bb64e82c
Zamira <keldibaevaz@gmail.com>
2025-07-18 23:02:17 +0300

    18.07.2025

---
+++ src/main/java/levelTwo/threads/TaskOneCounter.java
@@ -0,0 +1,26 @@
+package levelTwo.threads;
+
+/**
+ * Создать класс counter с одним полем
+ * private int counter
+ *
+ * добавить методы
+ * getCounter - для получ.поля каунт
+ * increment - для увеличения значения поля на единицу
+ * decrement - для уменьшения значения поля на единицу
+ */
+public class TaskOneCounter {
+    private int count;
+
+    public void increment() {
+       count++;
+    }
+
+    public void decrement() {
+        count--;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
