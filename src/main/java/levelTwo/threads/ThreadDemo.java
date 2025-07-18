9dedb7e6c57f47523b449a9830a1c2154a160df9
Zamira <keldibaevaz@gmail.com>
2025-07-18 22:55:59 +0300

    18.07.2025

--- src/main/java/levelTwo/threads/ThreadDemo.java
+++ src/main/java/levelTwo/threads/ThreadDemo.java
@@ -3,13 +3,18 @@ package levelTwo.threads;
 public class ThreadDemo {
     public static void main(String[] args) {
         var simpleThread = new SimpleThread();
+        System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
         var simpleRunnable = new Thread(new SimpleRunnable(), "My name");
         var lambda = new Thread(() -> System.out.println("Hello from lambda: " + Thread.currentThread().getName()));
         simpleThread.start();
+        System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
+
         simpleRunnable.start();
         lambda.start();
         try {
-            simpleThread.join();
+            simpleThread.join(100L);
+            System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
+
             simpleRunnable.join();
             lambda.join();
         } catch (InterruptedException e) {
