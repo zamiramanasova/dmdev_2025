b9ebdd9cb377b6b98f654c9f6791cefdb8231f42
Zamira <keldibaevaz@gmail.com>
2025-07-23 22:52:49 +0300

    23.07.2025

--- src/main/java/levelTwo/threads/volatileLesson/VolatileDemo.java
+++ src/main/java/levelTwo/threads/volatileLesson/VolatileDemo.java
@@ -1,4 +1,22 @@
 package levelTwo.threads.volatileLesson;
 
 public class VolatileDemo {
+
+    private static volatile boolean flag = false;
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+            while (!flag) {
+                System.out.println("still false");
+            }
+        });
+        thread1.start();
+
+        Thread thread2 = new Thread(() -> {
+            flag = true;
+            System.out.println("flag is set");
+        });
+        thread2.start();
+    }
+
 }
