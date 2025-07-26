38fcd86331abb5b1c990c32ff23d142c4eea6fd1
Zamira <keldibaevaz@gmail.com>
2025-07-26 21:20:49 +0300

    26.07.2025

--- src/main/java/levelTwo/threads/methods/BuyerThread.java
+++ src/main/java/levelTwo/threads/methods/BuyerThread.java
@@ -1,5 +1,13 @@
 package levelTwo.threads.methods;
 
+import java.util.Queue;
+
 public class BuyerThread implements Runnable {
 
+    private final Queue<Cashbox>
+
+    @Override
+    public void run() {
+
+    }
 }
---
+++ src/main/java/levelTwo/threads/methods/Cashbox.java
@@ -0,0 +1,16 @@
+package levelTwo.threads.methods;
+
+public class Cashbox {
+    private static int generator = 1;
+    private int id;
+    public Cashbox() {
+        this.id = generator++;
+    }
+
+    @Override
+    public String toString() {
+        return "Cashbox{" +
+                "id=" + id +
+                '}';
+    }
+}
