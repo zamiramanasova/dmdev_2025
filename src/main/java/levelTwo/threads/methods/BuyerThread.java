03fb154e9500c0e5819f6f718bc8f92df9065610
Zamira <keldibaevaz@gmail.com>
2025-07-26 21:35:46 +0300

    26.07.2025

--- src/main/java/levelTwo/threads/methods/BuyerThread.java
+++ src/main/java/levelTwo/threads/methods/BuyerThread.java
@@ -4,10 +4,31 @@ import java.util.Queue;
 
 public class BuyerThread implements Runnable {
 
-    private final Queue<Cashbox>
+    private final Queue<Cashbox> cashboxes;
 
+    public BuyerThread(Queue<Cashbox> cashboxes) {
+        this.cashboxes = cashboxes;
+    }
     @Override
     public void run() {
+        try {
+            while (true) {
+                if (!cashboxes.isEmpty()) {
+                    Cashbox cashbox = cashboxes.remove();
+                    System.out.println(Thread.currentThread().getName() + " обслуживание на кассе " + cashbox);
+
+                    Thread.sleep(5L);
+
+                    System.out.println(Thread.currentThread().getName() + " освобождаю кассу " + cashbox);
+                    cashboxes.add(cashbox);
+                } else {
+                    System.out.println(Thread.currentThread().getName() + " ожидает свободную кассу");
+                    Thread.sleep(5L);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }
---
+++ src/main/java/levelTwo/threads/methods/CashboxRunner.java
@@ -0,0 +1,7 @@
+package levelTwo.threads.methods;
+
+public class CashboxRunner {
+    public static void main(String[] args) {
+
+    }
+}
