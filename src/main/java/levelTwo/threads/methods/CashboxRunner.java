4cc6478e84c80266d4b017813100695386301370
Zamira <keldibaevaz@gmail.com>
2025-07-26 21:41:39 +0300

    26.07.2025

--- src/main/java/levelTwo/threads/methods/CashboxRunner.java
+++ src/main/java/levelTwo/threads/methods/CashboxRunner.java
@@ -1,7 +1,30 @@
 package levelTwo.threads.methods;
 
-public class CashboxRunner {
-    public static void main(String[] args) {
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+public class CashboxRunner {
+    public static void main(String[] args) throws InterruptedException {
+        Queue<Cashbox> cashboxes = new ArrayDeque<>(List.of(new Cashbox(), new Cashbox()));
+
+        List<Thread> threads = Stream.of(
+                new BuyerThread(cashboxes),
+                new BuyerThread(cashboxes),
+                new BuyerThread(cashboxes),
+                new BuyerThread(cashboxes),
+                new BuyerThread(cashboxes),
+                new BuyerThread(cashboxes),
+                new BuyerThread(cashboxes),
+                new BuyerThread(cashboxes)
+        )
+                .map(Thread::new)
+                .peek(Thread::start)
+                .collect(Collectors.toList());
+        for (Thread thread : threads) {
+            thread.join();
+        }
     }
 }
