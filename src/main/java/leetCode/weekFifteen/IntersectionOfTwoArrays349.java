c89c54b03be609d45e8029130df3f54edd48cb2f
Zamira <keldibaevaz@gmail.com>
2025-08-13 11:05:30 +0300

    13.08.2025

--- src/main/java/leetCode/weekFifteen/IntersectionOfTwoArrays349.java
+++ src/main/java/leetCode/weekFifteen/IntersectionOfTwoArrays349.java
@@ -14,14 +14,14 @@ public class IntersectionOfTwoArrays349 {
     public static void main(String[] args) {
         int[] nums1 = new int[] {1,2,2,1};
         int[] nums2 = new int[] {2,2};
-        System.out.println(Arrays.toString(intersection(nums1, nums2)));
+        System.out.println(Arrays.toString(intersection2(nums1, nums2)));
     }
 
     //method brute force
     public static int[] intersection(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
-        Set<Integer> set = new HashSet<Integer>();
+        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (nums1[i] == nums2[j]) {
@@ -29,6 +29,7 @@ public class IntersectionOfTwoArrays349 {
                 }
             }
         }
+        // O(n * m)
         int set_size = set.size();
         int[] answer = new int[set_size];
 
@@ -38,5 +39,36 @@ public class IntersectionOfTwoArrays349 {
             ind++;
         }
         return answer;
+    }
+
+    public static int[] intersection2(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        Set<Integer> set_1 = new HashSet<>();
+        Set<Integer> ans = new HashSet<>();
+
+        //весь цикл O(n)
+        for (int i = 0; i < n; i++) {
+            set_1.add(nums1[i]); //O(1) in average
+        }
+
+        //весь цикл O(m)
+        for (int i = 0; i < m; i++) {
+            if (set_1.contains(nums2[i])) { //O(1) in average
+                ans.add(nums2[i]); //O(1) in average
+            }
+        }
+
+        int[] answer = new int[ans.size()];
+
+        int ind = 0;
+        for (Integer x : ans) {
+            answer[ind] = x;
+            ind++;
+        }
+        return answer;
+        //общее
+        // tm comp = O(n + m)
+        // space comp O(n + m)
     }
 }
