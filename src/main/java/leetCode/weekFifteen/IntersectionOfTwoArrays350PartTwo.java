f11c33bf9dc363038473879c31e2d62ad07b6a5e
Zamira <keldibaevaz@gmail.com>
2025-08-13 11:13:33 +0300

    13.08.2025

---
+++ src/main/java/leetCode/weekFifteen/IntersectionOfTwoArrays350PartTwo.java
@@ -0,0 +1,34 @@
+package leetCode.weekFifteen;
+
+import java.util.*;
+
+public class IntersectionOfTwoArrays350PartTwo {
+    public static void main(String[] args) {
+        int[] nums1 = new int[] {1,2,2,1};
+        int[] nums2 = new int[] {2,2};
+        System.out.println(Arrays.toString(intersect(nums1, nums2)));
+    }
+
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (nums1[i] == nums2[j]) {
+                    ans.add(nums1[i]);
+                    nums2[j] = -1;
+                    break;
+                }
+            }
+        }
+        int[] answer = new int[ans.size()];
+
+        int ind = 0;
+        for (Integer x : ans) {
+            answer[ind] = x;
+            ind++;
+        }
+        return answer;
+    }
+}
