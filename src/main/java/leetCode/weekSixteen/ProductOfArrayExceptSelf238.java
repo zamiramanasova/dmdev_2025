53a0c8d170836b00e1c5598f179b55c54d02e010
Zamira <keldibaevaz@gmail.com>
2025-08-23 16:08:20 +0300

    23.08.2025

--- src/main/java/leetCode/weekSixteen/ProductOfArrayExceptSelf238.java
+++ src/main/java/leetCode/weekSixteen/ProductOfArrayExceptSelf238.java
@@ -1,10 +1,14 @@
 package leetCode.weekSixteen;
 
+import java.util.Arrays;
+//
 public class ProductOfArrayExceptSelf238 {
     public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
 
     }
-    public int[] productExceptSelf(int[] nums) {
+    public static int[] productExceptSelf(int[] nums) {
 
         // [a,b,c,d,e,f,g,h,i,j]
         //  префикс|i|suffix
@@ -19,6 +23,22 @@ public class ProductOfArrayExceptSelf238 {
         }
 
         int[] suffix_mul = new int[n];
+
+        suffix_mul[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix_mul[i] = suffix_mul[i + 1] * nums[i];
+        }
+
+        int[] answer = new int[n];
+        answer[0] = suffix_mul[1];
+        answer[n - 1] = prefix_mul[n - 2];
+        for (int i = 1; i < n - 1; i++) {
+            answer[i] = prefix_mul[i - 1] * suffix_mul[i + 1];
+        }
+
+        return answer;
+        // TIME: O(N)
+        // SPACE: O(N)
 
     }
 }
