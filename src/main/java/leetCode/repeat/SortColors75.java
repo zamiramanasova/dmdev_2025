603e89ac2ab60b893f86e4eb3bec7af34a8a02f4
Zamira <keldibaevaz@gmail.com>
2025-09-10 21:49:32 +0300

    10.09.2025

--- src/main/java/leetCode/repeat/SortColors75.java
+++ src/main/java/leetCode/repeat/SortColors75.java
@@ -1,7 +1,27 @@
 package leetCode.repeat;
 
+import java.util.Arrays;
+
 public class SortColors75 {
     public static void main(String[] args) {
+        int[] nums = new int[] {2,0,2,1,1,0};
+        sortColors(nums);
+        System.out.println(Arrays.toString(nums));
+    }
 
+    public static void sortColors(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int min = nums[i];
+            int minId = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < min) {
+                    min = nums[j];
+                    minId = j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = min;
+            nums[minId] = temp;
+        }
     }
 }
