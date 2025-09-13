3f85c4c5c09773748cb111b842b78b70c9c9df46
Zamira <keldibaevaz@gmail.com>
2025-09-13 16:36:55 +0300

    13.09.2025

--- src/main/java/leetCode/repeat/RecurtionOntheClass.java
+++ src/main/java/leetCode/repeat/RecurtionOntheClass.java
@@ -1,7 +1,34 @@
 package leetCode.repeat;
 
-public class RecurtionOntheClass {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class RecurtionOntheClass {
+    static int n;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        if (rec(1)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+    }
+
+    public static boolean rec(int x) {
+        if (x > n) {
+            return false;
+        } else if (x == n) {
+            return true;
+        }
+        //x <= n
+        //option 1. +3
+        if (rec(x + 5)) {
+            return true;
+        }
+        if (rec(x + 3)) {
+             return true;
+        }
+        return false;
     }
 }
