41ccc97f9d47281d8602cb86552c4c08d275fe7e
Zamira <keldibaevaz@gmail.com>
2025-09-16 22:47:10 +0300

    16.09.2025

--- src/main/java/fromBook/binarySearch/BinarySearch704.java
+++ src/main/java/fromBook/binarySearch/BinarySearch704.java
@@ -4,7 +4,7 @@ public class BinarySearch704 {
     public static void main(String[] args) {
         int[] nums = new int[] {-1,0,3,5,9,12};
         int target = 2;
-        System.out.println(search(nums, target));
+        System.out.println(search2(nums, target));
     }
 
     //brute force
@@ -18,5 +18,23 @@ public class BinarySearch704 {
         }
         return -1;
 
+    }
+
+    // БИНАРНЫЙ ПОИСК, решено с помощью ИИ
+    public static int search2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length -1;
+        int mid = 0;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
     }
 }
