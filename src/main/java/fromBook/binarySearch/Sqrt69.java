5bac0a8497d6a879460d6a52ef20ef8d45766694
Zamira <keldibaevaz@gmail.com>
2025-09-17 23:35:39 +0300

    17.09.2025

---
+++ src/main/java/fromBook/binarySearch/Sqrt69.java
@@ -0,0 +1,26 @@
+package fromBook.binarySearch;
+
+public class Sqrt69 {
+    public static void main(String[] args) {
+
+    }
+
+    public int mySqrt(int x) {
+        if (x < 2)
+            return x;
+        int left = 1;
+        int right = x / 2;
+        int ans = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return  ans;
+    }
+}
