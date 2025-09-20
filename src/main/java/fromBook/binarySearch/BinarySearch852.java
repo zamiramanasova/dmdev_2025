55d4cbb355898274418cd0dedc0706cb6ee1a731
Zamira <keldibaevaz@gmail.com>
2025-09-20 20:19:28 +0300

    20.09.2025

--- src/main/java/fromBook/binarySearch/BinarySearch852.java
+++ src/main/java/fromBook/binarySearch/BinarySearch852.java
@@ -2,6 +2,28 @@ package fromBook.binarySearch;
 
 public class BinarySearch852 {
     public static void main(String[] args) {
+        int[] nums = new int[] {0,1,0};
 
+        System.out.println(peakIndexInMountainArray(nums));
+    }
+
+    public static int peakIndexInMountainArray(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] <= arr[mid + 1]) {
+                left = mid + 1; // ищем справа
+            } else {
+                right = mid - 1; // ищем слева
+            }
+        }
+        return left;
     }
 }
+/**
+ * Time Complexity: O(log N)
+ * ✅ Space Complexity: O(1)
+ */
