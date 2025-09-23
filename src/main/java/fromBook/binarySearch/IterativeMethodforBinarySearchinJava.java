ef594ec38ed3c9e987903645d173f2bf97296b9d
Zamira <keldibaevaz@gmail.com>
2025-09-23 22:05:17 +0300

    23.09.2025

--- src/main/java/fromBook/binarySearch/IterativeMethodforBinarySearchinJava.java
+++ src/main/java/fromBook/binarySearch/IterativeMethodforBinarySearchinJava.java
@@ -2,6 +2,45 @@ package fromBook.binarySearch;
 
 public class IterativeMethodforBinarySearchinJava {
     public static void main(String[] args) {
+        int a[] = { 2, 3, 4, 10, 40 };
+        int n = a.length;
+        int x = 10;
 
+        int res = binarySearch(a, 0, n - 1, x);
+
+        System.out.println("Element to be searched is : "+ x);
+
+        if (res == -1)
+            System.out.println("Element is not present in array");
+        else
+            System.out.println("Element is present at index: " + res);
     }
+
+    public static int binarySearch(int a[], int l, int r, int x)
+    {
+        while (l <= r) {
+            int m = (l + r) / 2;
+
+            // Index of Element Returned
+            if (a[m] == x) {
+                return m;
+
+                // If element is smaller than mid, then
+                // it can only be present in left subarray
+                // so we decrease our r pointer to mid - 1
+            } else if (a[m] > x) {
+                r = m - 1;
+
+                // Else the element can only be present
+                // in right subarray
+                // so we increase our l pointer to mid + 1
+            } else {
+                l = m + 1;
+            }
+        }
+
+        // No Element Found
+        return -1;
+    }
+
 }
