2895865c3a8756fbe8ecbc521b214b0ee2bf2597
Zamira <keldibaevaz@gmail.com>
2025-09-24 21:40:58 +0300

    24.09.2025

---
+++ src/main/java/fromBook/binarySearch/NeetCode150.java
@@ -0,0 +1,25 @@
+package fromBook.binarySearch;
+
+/**
+ * Binary Search
+ * Solved
+ * You are given an array of distinct integers nums,
+ * sorted in ascending order, and an integer target.
+ *
+ * Implement a function to search for target within nums.
+ * If it exists, then return its index, otherwise, return -1.
+ *
+ * Your solution must run in
+ * O(logn) time.
+ *
+ * Example 1:
+ *
+ * Input: nums = [-1,0,2,4,6,8], target = 4
+ *
+ * Output: 3
+ */
+public class NeetCode150 {
+    public static void main(String[] args) {
+
+    }
+}
