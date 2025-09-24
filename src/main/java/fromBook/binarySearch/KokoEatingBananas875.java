9e9493b676a1f365c1b93a9ce19dbe029d31bdb5
Zamira <keldibaevaz@gmail.com>
2025-09-24 22:47:12 +0300

    24.09.2025

--- src/main/java/fromBook/binarySearch/KokoEatingBananas875.java
+++ src/main/java/fromBook/binarySearch/KokoEatingBananas875.java
@@ -2,7 +2,28 @@ package fromBook.binarySearch;
 
 public class KokoEatingBananas875 {
     public static void main(String[] args) {
-
+        int[] piles = {3, 6, 7, 11};
+        int h = 8;
+        System.out.println(minEatingSpeed(piles, h)); // должно вывести 4
     }
 
+    public static int minEatingSpeed(int[] piles, int h) {
+        int left = 1; //девый указатель
+        int right = 0;//правый
+        for (int pile : piles) right = Math.max(right, pile); //вычисление все кучи
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int time = 0;//время
+            for (int pile : piles) {// проверяем, успевает ли Коко съесть все бананы за h часов
+                time += (pile + mid - 1) / mid;
+            }
+            if (time <= h) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
 }
