f6e3c91d1591b706bf149fe42cacb8bca7f08ae8
Zamira <keldibaevaz@gmail.com>
2026-03-19 22:19:49 +0600

    refactoring

--- src/main/java/fromBook/binarySearch/BinarySearch35.java
+++ src/main/java/fromBook/binarySearch/BinarySearch35.java
@@ -34,7 +34,6 @@ public class BinarySearch35 {
  * Подсказка:
  * Это всё тот же бинарный поиск, только:
  * Если nums[mid] == target → вернуть mid.
-
  * Если nums[mid] < target → нужно двигать левую границу (left = mid + 1).
  *
  * Если nums[mid] > target → нужно двигать правую границу (right = mid - 1).
