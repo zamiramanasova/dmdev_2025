7a5af1ff05ff64eda09e636272915f36d3fbddf5
Zamira <keldibaevaz@gmail.com>
2026-03-23 17:24:53 +0600

    refactoring

--- src/main/java/fromBook/binarySearch/NeetCode150.java
+++ src/main/java/fromBook/binarySearch/NeetCode150.java
@@ -9,7 +9,7 @@ package fromBook.binarySearch;
  * If it exists, then return its index, otherwise, return -1.
  * Your solution must run in
  * O(logn) time.
- *
+
  * Example 1:
  *
  * Input: nums = [-1,0,2,4,6,8], target = 4
