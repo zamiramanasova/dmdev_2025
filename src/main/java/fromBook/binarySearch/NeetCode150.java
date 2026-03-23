0b429de566f976590e5c7475d5b6fe6a165d3745
Zamira <keldibaevaz@gmail.com>
2026-03-23 17:24:25 +0600

    refactoring

--- src/main/java/fromBook/binarySearch/NeetCode150.java
+++ src/main/java/fromBook/binarySearch/NeetCode150.java
@@ -7,7 +7,7 @@ package fromBook.binarySearch;
  * sorted in ascending order, and an integer target.
  * Implement a function to search for target within nums.
  * If it exists, then return its index, otherwise, return -1.
- *
+
  * Your solution must run in
  * O(logn) time.
  *
