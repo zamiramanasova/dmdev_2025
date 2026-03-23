5cbb4a0d19e8994205aec0bdb08166bf2dc0dea3
Zamira <keldibaevaz@gmail.com>
2026-03-23 17:23:57 +0600

    refactoring

--- src/main/java/fromBook/binarySearch/NeetCode150.java
+++ src/main/java/fromBook/binarySearch/NeetCode150.java
@@ -5,7 +5,7 @@ package fromBook.binarySearch;
  * Solved
  * You are given an array of distinct integers nums,
  * sorted in ascending order, and an integer target.
- *
+
  * Implement a function to search for target within nums.
  * If it exists, then return its index, otherwise, return -1.
  *
