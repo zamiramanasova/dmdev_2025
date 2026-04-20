62e5a05f0b45ffad66c2e842e60edfcd4df540b1
Zamira <keldibaevaz@gmail.com>
2026-04-20 15:59:48 +0600

    refactoring 20/04/26

--- src/main/java/leetCode/weekThirteen/AddBinary67.java
+++ src/main/java/leetCode/weekThirteen/AddBinary67.java
@@ -45,7 +45,7 @@ public class AddBinary67 {
     // 1 + 1 + 0 = 2 → append 0, carry = 1
     // i = 0, j = -1
     // 1 + 0 + 1 = 2 → append 0, carry = 1
-    //
+
     // i = -1, j = -1
     //
     // 0 + 0 + 1 = 1 → append 1
