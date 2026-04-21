a71c0470235415af3a326bf6d04bc77fc73371c9
Zamira <keldibaevaz@gmail.com>
2026-04-21 22:55:51 +0600

    refactoring 22/04/26

--- src/main/java/leetCode/weekThirteen/LongestPalindrome409.java
+++ src/main/java/leetCode/weekThirteen/LongestPalindrome409.java
@@ -24,7 +24,6 @@ public class LongestPalindrome409 {
         // буквы (например, 2x 'a', 2x 'b').
         // Можно включить только одну букву с нечётным
         // количеством в центр палиндрома.
-
         //считаем количество каждой буквы
         Map<Character, Integer> count = new HashMap<>();
         for (char c : s.toCharArray()) {
