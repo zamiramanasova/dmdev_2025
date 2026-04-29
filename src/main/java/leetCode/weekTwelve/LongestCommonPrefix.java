3b3dbb987ec05b841743c9aacc537ebbbb2f113c
Zamira <keldibaevaz@gmail.com>
2026-04-29 13:13:17 +0600

    refactoring 29/04/26

--- src/main/java/leetCode/weekTwelve/LongestCommonPrefix.java
+++ src/main/java/leetCode/weekTwelve/LongestCommonPrefix.java
@@ -26,7 +26,7 @@ public class LongestCommonPrefix {
         // Цель: проверить, подходит ли текущий префикс к каждой следующей строке.
         for (int i = 1; i < strs.length; i++) {
             // Пока строка strs[i] не начинается с prefix, будем укорачивать его.
-            //Метод startsWith() проверяет, является ли prefix началом строки.
+            // Метод startsWith() проверяет, является ли prefix началом строки.
             //Пример:
             //i = 1, strs[1] = "flow"
             //"flow".startsWith("flower") ❌
