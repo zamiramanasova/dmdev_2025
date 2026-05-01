ed3d22c1b9f1df0b54af55ed184f3e521b32c5de
Zamira <keldibaevaz@gmail.com>
2026-05-01 11:06:16 +0600

    refactoring 30/05/26

--- src/main/java/leetCode/weekTwelve/RemoveDuplicatesFromSortedArrayII.java
+++ src/main/java/leetCode/weekTwelve/RemoveDuplicatesFromSortedArrayII.java
@@ -50,7 +50,7 @@ public class RemoveDuplicatesFromSortedArrayII {
         // Процесс будет таким:
         // i = 0, n = 1 → i < 2 → пишем → nums[0] = 1
         // i = 1, n = 1 → i < 2 → пишем → nums[1] = 1
-        //
+
         // i = 2, n = 1 → nums[0] = 1 → n == nums[i - 2] → пропускаем
         //
         // i = 2, n = 2 → 2 != 1 → пишем → nums[2] = 2
