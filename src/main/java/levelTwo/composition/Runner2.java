68fded8f9adeb3ce816831a01850a3cdcd2a5156
Zamira <keldibaevaz@gmail.com>
2026-05-06 13:28:46 +0600

    refactoring 06/05/26

--- src/main/java/levelTwo/composition/Runner2.java
+++ src/main/java/levelTwo/composition/Runner2.java
@@ -2,7 +2,7 @@ package levelTwo.composition;
 
 public class Runner2 {
     public static void main(String[] args) {
-        //laptop -> computer -> object
+        // laptop -> computer -> object
         new LapTop();
         System.out.println("====");
         new LapTop();
