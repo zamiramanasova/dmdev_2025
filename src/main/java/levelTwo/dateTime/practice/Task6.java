24b97d91ed9bd6b4729a609d47b1b7dad7070191
Zamira <keldibaevaz@gmail.com>
2026-05-06 13:30:33 +0600

    refactoring 06/05/26

--- src/main/java/levelTwo/dateTime/practice/Task6.java
+++ src/main/java/levelTwo/dateTime/practice/Task6.java
@@ -3,7 +3,7 @@ package levelTwo.dateTime.practice;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.Period;
+
 import java.time.temporal.ChronoUnit;
 
 /**
