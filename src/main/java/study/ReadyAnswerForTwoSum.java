9802f06793d8cb7fd5f6a036a5d1decd7bbf076f
Zamira <keldibaevaz@gmail.com>
2026-05-11 11:03:38 +0600

    refactoring 11/05/26

--- src/main/java/study/ReadyAnswerForTwoSum.java
+++ src/main/java/study/ReadyAnswerForTwoSum.java
@@ -87,7 +87,7 @@ public class ReadyAnswerForTwoSum {
             // arrayK[3] = [2]
             // arrayK[4] = [3]
             // Теперь в цикле:
-            //Когда i = 1:
+            // Когда i = 1:
             //k - i = 4
             //arrayK[1] = [0], arrayK[4] = [3]
             //✅ оба не пусты → ищем все пары [0,3]
