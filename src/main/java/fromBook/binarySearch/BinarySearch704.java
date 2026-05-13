fb549da0a026c22f727c6f1b2eca665c35a41317
Zamira <keldibaevaz@gmail.com>
2026-05-13 23:30:44 +0600

    refactoring 13/05/26 new feature

--- src/main/java/fromBook/binarySearch/BinarySearch704.java
+++ src/main/java/fromBook/binarySearch/BinarySearch704.java
@@ -7,7 +7,7 @@ public class BinarySearch704 {
         System.out.println(search(nums, target));
     }
 
-    //brute force
+    // brute force
     public static int search(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == target) {
