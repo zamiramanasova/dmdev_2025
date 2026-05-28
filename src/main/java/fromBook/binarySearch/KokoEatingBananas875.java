0218f795a227b9ba26b643a9f84dbadcf04317ed
Zamira <keldibaevaz@gmail.com>
2026-05-28 23:27:48 +0600

    28/05/26 new feature part one

--- src/main/java/fromBook/binarySearch/KokoEatingBananas875.java
+++ src/main/java/fromBook/binarySearch/KokoEatingBananas875.java
@@ -11,7 +11,7 @@ public class KokoEatingBananas875 {
 
         int left = 1; // левый указатель
         int right = 0;// правый
-        for (int pile : piles) right = Math.max(right, pile); //вычисление все кучи
+        for (int pile : piles) right = Math.max(right, pile); // вычисление все кучи
 
         while (left < right) {
             int mid = left + (right - left) / 2;
