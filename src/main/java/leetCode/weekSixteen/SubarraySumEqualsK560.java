1d2570a3bc100f67b1f1f6832bac8fb8d3e3ae42
Zamira <keldibaevaz@gmail.com>
2026-05-30 09:35:33 +0600

    28/05/26 new feature part one

--- src/main/java/leetCode/weekSixteen/SubarraySumEqualsK560.java
+++ src/main/java/leetCode/weekSixteen/SubarraySumEqualsK560.java
@@ -16,7 +16,7 @@ public class SubarraySumEqualsK560 {
          * subarray(nums, L, R) = nums[L .. R]
          * subbarray(nums, L=2, R=2, R=4) = [nums[2], nums[3], nums[4]]
          * sum(subarray(nums, L, R)) =
-         *  sum(subarray(nums, 0, R)) - sum(subarray(nums, 0, L-1))
+         * sum(subarray(nums, 0, R)) - sum(subarray(nums, 0, L-1))
          *  (nums[0] + nums[1] + ... + nums[R] - (nums[0] + nums[1] + ... + nums[L-1])
          *  prefix_sum[R]                      - prefix_sum[L-1]
          *  = nums[L] + nums[L+1] + ... + nums[R]
